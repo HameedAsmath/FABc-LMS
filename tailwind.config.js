5514670b43dfbc3e8e713298cff1eacf40564deb
HameedAsmath <hameedasmath2001@gmail.com>
2023-08-28 13:34:41 +0000

    Navbar, Footer added and home page incomplete

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          FABcYellow: "#FFBA00",
+          FABcBlue: "#0087A5",
+          FABcGray: "#6F6666",
+          FABcLightBlue: "#E6F4F6",
+        }
+      },
+    },
+    plugins: [],
+  }
